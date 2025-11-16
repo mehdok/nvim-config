095d2a3c4030ba1a02f59e6852e1ace50b5f6f64
Mehdi Sohrabi <mehdok@gmail.com>
2025-11-16 15:25:14 +0100

    add more plugin;

--- init.lua
+++ init.lua
@@ -42,5 +42,6 @@ require("lazy").setup({
 	require("plugins.gitsigns"),
 	require("plugins.alpha"),
 	require("plugins.indent-blankline"),
+	require("plugins.misc"),
 	-- require 'plugins.indent_line'
 })
